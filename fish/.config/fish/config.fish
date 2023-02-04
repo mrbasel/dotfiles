9c14050658f5c3d14b12c6f6bb62851cae404bf6
Basel <mrbasel555@gmail.com>
2023-02-04 22:13:06 +0300

    Some changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,13 +3,27 @@
 # Enable vi key bindings
 fish_vi_key_bindings
 
+# Cursor shape 
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+
 # Fix control-F issue: https://stackoverflow.com/questions/32037197/ctrl-f-no-longer-works-to-accept-suggestions-why
 bind -M insert \cf accept-autosuggestion
 
+# Aliases
 alias t='tmux'
 alias ff='fzf'
 alias http='python3 -m http.server'
 alias copy='xclip -sel clip'
+
+# Git aliases
+alias gits='git status'
+alias gitd='git diff'
+alias gita='git add'
+alias gitc='git commit'
+alias gitp='git push'
 
 # Fzf aliases
 alias sd='cd (fd --type directory | fzf)' # search directories
