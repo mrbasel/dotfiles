dd835519ccac30654397e3001be23f571300da55
Basel <mrbasel555@gmail.com>
2023-04-12 21:59:08 +0300

    nvim: add smartpairs plugin

--- nvim/.config/nvim/lua/basel/packer.lua
+++ nvim/.config/nvim/lua/basel/packer.lua
@@ -113,6 +113,8 @@ require('packer').startup(function(use)
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
+    -- Auto close pairs
+    use 'gosukiwi/vim-smartpairs'
 
 
     -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
