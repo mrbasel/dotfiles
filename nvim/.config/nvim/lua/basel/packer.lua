fe7fa5074572655ca2351b7bb576d786a7569e77
Basel <mrbasel555@gmail.com>
2023-04-12 22:00:12 +0300

    nvim: format

--- nvim/.config/nvim/lua/basel/packer.lua
+++ nvim/.config/nvim/lua/basel/packer.lua
@@ -19,7 +19,7 @@ require('packer').startup(function(use)
     -- Package manager
     use 'wbthomason/packer.nvim'
 
-    use {'neoclide/coc.nvim', branch='release'}
+    use { 'neoclide/coc.nvim', branch = 'release' }
 
     use { -- LSP Configuration & Plugins
         'neovim/nvim-lspconfig',
@@ -72,7 +72,7 @@ require('packer').startup(function(use)
     }
 
     -- Highlight word under cursor
-    use {'RRethy/vim-illuminate'}
+    use { 'RRethy/vim-illuminate' }
 
     -- File explorer
     use {
@@ -80,7 +80,7 @@ require('packer').startup(function(use)
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+        tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
     }
 
     -- Bufferline
@@ -102,12 +102,12 @@ require('packer').startup(function(use)
     use 'AlexvZyl/nordic.nvim'
     use { "ellisonleao/gruvbox.nvim" }
 
-    use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+    use 'nvim-lualine/lualine.nvim'           -- Fancier statusline
     use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-    use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-    use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-    
-    use { 'fannheyward/telescope-coc.nvim' } -- Integrate telescope with coc.nvim
+    use 'numToStr/Comment.nvim'               -- "gc" to comment visual regions/lines
+    use 'tpope/vim-sleuth'                    -- Detect tabstop and shiftwidth automatically
+
+    use { 'fannheyward/telescope-coc.nvim' }  -- Integrate telescope with coc.nvim
     -- Fuzzy Finder (files, lsp, etc)
     use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
     -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
@@ -115,7 +115,6 @@ require('packer').startup(function(use)
 
     -- Auto close pairs
     use 'gosukiwi/vim-smartpairs'
-
 
     -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
     local has_plugins, plugins = pcall(require, 'custom.plugins')
