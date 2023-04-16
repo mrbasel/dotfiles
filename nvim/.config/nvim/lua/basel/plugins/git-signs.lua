804b4e8333a98f4847b3260a193273e09a73ad61
Basel <mrbasel555@gmail.com>
2023-04-16 08:18:19 +0300

    nvim: relative time in current line blame

--- nvim/.config/nvim/lua/basel/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/basel/plugins/git-signs.lua
@@ -9,4 +9,5 @@ require('gitsigns').setup {
         changedelete = { text = '~' },
     },
     current_line_blame = true,
+    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
 }
